3d75d6c04f641e727cde161228c69f71a9f08b49
baehyunsol <baehyunsol@github.com>
2023-07-01 16:03:09 +0900

    add missing files

---
+++ src/arg_def.rs
@@ -0,0 +1,39 @@
+use crate::err::ParseError;
+use crate::expr::Expr;
+use crate::session::InternedString;
+use crate::span::Span;
+use crate::token::{OpToken, TokenKind, TokenList};
+
+// TODO: where should this module belong?
+
+pub struct ArgDef {
+    name: InternedString,
+    type_: Box<Expr>,   // TODO: better implementation?
+}
+
+// NAME ':' TYPE
+pub fn parse_arg_def(tokens: &mut TokenList) -> Result<ArgDef, ParseError> {
+    assert!(!tokens.is_eof(), "Internal Compiler Error 53A2FA7");
+
+    let name = match tokens.step() {
+        Some(token) if token.is_identifier() => token.unwrap_identifier(),
+        Some(token) => {
+            return Err(ParseError::tok(
+                token.kind.clone(), token.span,
+                vec![TokenKind::Identifier(InternedString::dummy())]
+            ));
+        }
+        None => unreachable!()
+    };
+
+    tokens.consume_token_or_error(TokenKind::Operator(OpToken::Colon))?;
+
+    let type_ = match tokens.step_type() {
+        Some(t) => Box::new(t?),
+        None => {
+            return Err(ParseError::eoe(Span::dummy()));
+        }
+    };
+
+    Ok(ArgDef { name, type_ })
+}
---
+++ src/session/interned_string.rs
@@ -0,0 +1,34 @@
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
+pub struct InternedString(u32);
+
+const DUMMY_INDEX: u32 = u32::MAX - 8;
+
+impl InternedString {
+
+    pub fn dummy() -> Self {
+        InternedString(DUMMY_INDEX)
+    }
+
+    pub fn is_dummy(&self) -> bool {
+        self.0 == DUMMY_INDEX
+    }
+
+}
+
+impl From<usize> for InternedString {
+
+    fn from(n: usize) -> Self {
+        assert!(n as u32 != DUMMY_INDEX, "Internal Compiler Error B2CC601");
+
+        InternedString(n as u32)
+    }
+
+}
+
+impl From<InternedString> for usize {
+
+    fn from(s: InternedString) -> usize {
+        s.0 as usize
+    }
+
+}
