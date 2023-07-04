8008aac09b2afd4460326f62fc5dd120e25bd153
baehyunsol <baehyunsol@github.com>
2023-07-04 16:42:47 +0900

    implementing `use` statements

---
+++ src/stmt/tests/use_.rs
@@ -0,0 +1,107 @@
+use std::collections::HashSet;
+
+#[test]
+fn test_parse_use() {}
+
+/*
+ * `use A.B;` -> `use A.B as B;`
+ * `use A.B.C;` -> `use A.B.C as C;`
+ * `use A.B, C.D;` -> `use A.B; use C.D;`
+ * `use {A.B, C.D};` -> `use A.B; use C.D;`
+ * `use A.{B, C, D};` -> `use A.B; use A.C; use A.D;`
+ * `use A.B, C, D;` -> `use A.B; use C; use D;`
+ * `use A.{B as C, D as E};` -> `use A.B as C; use A.D as E;`
+ * `use A.{B, C} as D;` -> Invalid
+ */
+fn sample() -> Vec<(String, HashSet<String>)> {
+    vec![
+        (
+            "use A.B;",
+            vec![
+                "use A.B as B;",
+            ],
+        ),
+        (
+            "use A.B.C;",
+            vec![
+                "use A.B.C as C;",
+            ],
+        ),
+        (
+            "use A.B.C as C;",
+            vec![
+                "use A.B.C as C;",
+            ],
+        ),
+        (
+            "use A.B, C.D;",
+            vec![
+                "use A.B as B;",
+                "use C.D as D;",
+            ],
+        ),
+        (
+            "use {A.B, C.D};",
+            vec![
+                "use A.B as B;",
+                "use C.D as D;",
+            ],
+        ),
+        (
+            "use A.{B, C, D};",
+            vec![
+                "use A.B as B;",
+                "use A.C as C;",
+                "use A.D as D;",
+            ],
+        ),
+        (
+            "use A.{B, C, D}, E.F;",
+            vec![
+                "use A.B as B;",
+                "use A.C as C;",
+                "use A.D as D;",
+                "use E.F as F;",
+            ],
+        ),
+        (
+            "use A.{B, C, D.{E, F, G}, H};",
+            vec![
+                "use A.B as B;",
+                "use A.C as C;",
+                "use A.D.E as E;",
+                "use A.D.F as F;",
+                "use A.D.G as G;",
+                "use A.H as H;",
+            ],
+        ),
+        (
+            "use A.B, C, D;",
+            vec![
+                "use A.B as B;",
+                "use C as C;",
+                "use D as D;",
+            ],
+        ),
+        (
+            "use A.{B as C, D as E};",
+            vec![
+                "use A.B as C;",
+                "use A.D as E;",
+            ],
+        ),
+    ].into_iter().map(
+        |(before, after)| (
+            before.to_string(),
+            after.into_iter().map(|s| s.to_string()).collect::<HashSet<String>>()
+        )
+    ).collect()
+}
+
+fn invalid() -> Vec<String> {
+    vec![
+        "use A.{B, C} as D;",
+    ].into_iter().map(
+        |s| s.to_string()
+    ).collect()
+}
