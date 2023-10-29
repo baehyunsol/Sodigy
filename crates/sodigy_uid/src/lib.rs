cd0e96d15d3049afdd7be23845bc1b2f1d1a681a
baehyunsol <baehyunsol@github.com>
2023-10-29 14:54:03 +0900

    uids

---
+++ crates/sodigy_uid/src/lib.rs
@@ -0,0 +1,17 @@
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+pub struct Uid(u128);
+
+// first 8 bits: Type of Uid
+// next 8 bits: Metadata
+// last 112 bits: Random Index
+
+const ERASER: u128 = 0x0000_ffff_ffff_ffff_ffff_ffff_ffff_ffff;
+
+// Uid types
+const SCOPE_BLOCK: u128 = 0xff << 120;
+
+impl Uid {
+    pub fn new_scope() -> Self {
+        Uid(rand::random::<u128>() & ERASER | SCOPE_BLOCK)
+    }
+}
