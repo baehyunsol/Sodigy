4ebae0556d5747a182090a726b04fd462913fb47
baehyunsol <baehyunsol@naver.com>
2026-08-06 19:22:20 +0900

    update test viewer

--- tests/viewer/src/commit.rs
+++ tests/viewer/src/commit.rs
@@ -1,5 +1,6 @@
-use crate::html_template;
+use crate::{escape_html, html_template};
 use sodigy_compiler_test::meta::git::CommitInfo;
+use sodigy_compiler_test::subprocess;
 
 pub fn render_commit(commit: &CommitInfo) -> String {
     let abbrev_hash = commit.commit_hash.get(0..9).unwrap().to_string();
@@ -11,15 +12,19 @@ pub fn render_commit(commit: &CommitInfo) -> String {
     };
     let author = &commit.author;
     let author_email = &commit.author_email;
-    let parent = match &commit.parent_hash {
+    let (parent, diff) = match &commit.parent_hash {
         Some(parent) => {
-            let abbrev_hash = parent.get(0..9).unwrap().to_string();
-            format!(r#"<li><a href="{abbrev_hash}.html">parent</a></li>"#)
+            let parent_hash = parent.get(0..9).unwrap().to_string();
+            (
+                format!(r#"<li><a href="{parent_hash}.html">parent</a></li>"#),
+                format!(
+                    r#"<pre class="code-block"><code>{}</code></pre>"#,
+                    escape_html(&diff_commits(&parent_hash, &abbrev_hash)),
+                ),
+            )
         },
-        None => String::new(),
+        None => (String::new(), String::new()),
     };
-
-    // TODO: `git diff -U5 --color=always --diff-algorithm=patience <hash1> <hash2>`
 
     html_template(
         &format!(r#"
@@ -34,7 +39,50 @@ pub fn render_commit(commit: &CommitInfo) -> String {
 </ul>
 
 {body}
+
+{diff}
+
 "#),
         true,
     )
+}
+
+fn diff_commits(prev: &str, next: &str) -> String {
+    let o = subprocess::run(
+        "git",
+        &[
+            "diff",
+            "-U5",
+            "--diff-algorithm=patience",
+            "--color=never",
+            prev,
+            next,
+        ],
+        ".",
+        5.0,
+        false,
+        true,
+    ).unwrap();
+    let s = String::from_utf8_lossy(&o.stdout);
+    let mut colored = vec![];
+
+    for line in s.lines() {
+        if line.starts_with("+") {
+            colored.push(format!("\x1b[32m{line}\x1b[0m"));
+        }
+
+        else if line.starts_with("-") {
+            colored.push(format!("\x1b[31m{line}\x1b[0m"));
+        }
+
+        else if line.starts_with("@") {
+            colored.push(format!("\x1b[33m{line}\x1b[0m"));
+        }
+
+        else {
+            colored.push(line.to_string());
+        }
+    }
+
+    colored.join("\n")
 }
